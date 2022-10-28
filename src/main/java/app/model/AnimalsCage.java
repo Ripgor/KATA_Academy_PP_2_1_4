b4d64b50a7774b8fbbb55e45d3dbf9a1acfa6ac2
Calculator <eschigreev@gmail.com>
2022-10-28 18:47:19 +0700

    First Create bean, fix dependencies

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,30 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
 
-    @Autowired
-    private Animal animal;
+    private Animal animal;      // Убрал @Autowired, с ней не работает
+    private final Timer timer;
+
+    // Связываем по имени через параметры конструктора
+    public AnimalsCage(@Qualifier("dog") Animal animal, Timer timer) {
+        this.animal = animal;
+        this.timer = timer;
+    }
+
+    public Timer getTimer() {
+        return timer;
+    }       // это для проверки в MainTest
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());        // мы должны вызывать таймер у уже существующего бина
         System.out.println("________________________");
     }
 }
